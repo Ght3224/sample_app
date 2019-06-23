a729c4c1a3980f2e22b24656de89f91eee6ae90e
YOUR NAME <adien330@yahoo.com>
2019-06-23 12:42:35 -0400

    played with view files from controller

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,18 +1,25 @@
 class Api::ExamplePagesController < ApplicationController
-  
+
+
+#place for ruby logic
+
+
   def the_good_morning_method 
-    render json: {message: "good morning"}
+    render 'first.json.jb'
+
+
+    #json: {message: "good morning", time: Time.now.strftime("%B" "%d" + "," + "%Y")}
   end
 
 
-  def the_good_afternoon_method 
-    render json: [{message: "good afternoon"}, {message: "tov yom"}]
-  end 
+  # def the_good_afternoon_method 
+  #   render json: [{message: "good afternoon"}, {message: "tov yom"}]
+  # end 
 
 
-  def the_good_evening_method 
-    render json: {message: "night night"}
-  end 
+  # def the_good_evening_method 
+  #   render json: {message: "night night"}
+  # end 
 
 
 end 
