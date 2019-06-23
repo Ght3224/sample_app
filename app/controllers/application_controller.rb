eeb0c41b94c69e6491648ed2b96ee6b5acd08918
YOUR NAME <adien330@yahoo.com>
2019-06-23 12:27:52 -0400

    playing around with controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session
+
+
+    def the_good_morning_method 
+    render json: {message: "good morning"}
+  end
+
+
+  def the_good_afternoon_method 
+    render json: [{message: "good afternoon"}, {message: "tov yom"}]
+  end 
+
+
+  def the_good_evening_method 
+    render json: {message: "night night"}
+  end 
+
+
 end
